702f038669361671d290b64b42684d78a0f7062e
Francisco Villena <francisco.villena@lpsystems.com>
2021-03-09 00:20:44 +0000

    Initial Commit

---
+++ BlazorLoginRedirect/BlazorLoginRedirect.Web/Components/RedirectToLogin.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+
+namespace BlazorLoginRedirect.Web.Components
+{
+    public class RedirectToLogin : ComponentBase
+    {
+        [Inject]
+        protected NavigationManager NavigationManager { get; set; }
+
+        protected override void OnInitialized()
+        {
+            try
+            {
+                NavigationManager.NavigateTo("Identity/Account/Login", true);
+            }
+            catch(Exception)
+            {
+
+            }
+        }
+    }
+}
